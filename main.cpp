2d52b7c97b08e79fae2153d8af644be995871aec
JayDiddyThaGOAT <jalenjacksoncs@gmail.com>
2019-12-08 18:24:31 -0800

    Calcuated Shortest Path From Head of Snake to Apple

--- main.cpp
+++ main.cpp
@@ -11,21 +11,22 @@ int height = COUNT * SIZE;
 char snakeDirection;
 int snakeLength;
 std::vector<int> snake;
-bool dead = false;
 
-bool snakeAI = false;
+bool snakeAI = true;
+std::vector<int> snakePath;
+std::string snakePathDirections;
+int snakePathIndex;
 
 int apple;
 int newTail;
 bool eaten;
 
-bool gameBegan, paused;
+bool gameBegan, paused, dead;
 
 struct Cell
 {
 	int x, y;
 	char direction;
-	bool visited;
 	bool solid;
 };
 
@@ -35,126 +36,214 @@ sf::Texture emptyTexture, appleTexture, snakeHeadTexture, snakeBodyTexture;
 sf::Sprite emptySprite, appleSprite, snakeHeadSprite, snakeBodySprite;
 sf::Font font;
 
-int ManhattanDistance(int a, int b)
+int ManhattanDistance(int a, int b, Cell grid[])
 {
-	return abs(cells[b].x - cells[a].x) + abs(cells[b].y - cells[a].y);
+	return abs(grid[b].x - grid[a].x) + abs(grid[b].y - grid[a].y);
 }
 
-std::vector<int> NeighborsOf(int root)
+std::vector<int> NeighborsOf(int root, Cell grid[])
 {
 	std::vector<int> neighbors;
 
-	if (cells[root - COUNT].y >= 0)
+	if (grid[root - COUNT].y >= 0)
 		neighbors.push_back(root - COUNT);
 
-	if (cells[root + 1].x < COUNT - 1)
+	if (grid[root + 1].x < COUNT - 1)
 		neighbors.push_back(root + 1);
 
-	if (cells[root + COUNT].y < COUNT - 1)
+	if (grid[root + COUNT].y < COUNT - 1)
 		neighbors.push_back(root + COUNT);
 
-	if (cells[root - 1].x >= 0)
+	if (grid[root - 1].x >= 0)
 		neighbors.push_back(root - 1);
 
 
 	return neighbors;
 }
 
+std::vector<int> ShortestPathBetween(int start, int end, Cell grid[])
+{
+	std::vector<int> path;
+
+	std::stack<int> open;
+	open.push(start);
+
+	while (!open.empty())
+	{
+		int current = open.top();
+		open.pop();
+
+		path.push_back(current);
+
+		if (current == end)
+			return path;
+
+		std::vector<int> currentNeighbors = NeighborsOf(current, grid);
+		int minNeighbor = -1;
+		int minDistance = INT_MAX;
+		for (auto& neighbor : currentNeighbors)
+		{
+			int distance = ManhattanDistance(neighbor, end, grid);
+			if (distance < minDistance)
+			{
+				minNeighbor = neighbor;
+				minDistance = distance;
+			}
+		}
+
+		if (minNeighbor < 0)
+			break;
+
+		open.push(minNeighbor);
+	}
+
+	path.clear();
+	return path;
+}
+
+std::string GetPathDirections(std::vector<int> path, Cell grid[])
+{
+	std::string directions;
+
+	int i = 0;
+	int j = 1;
+
+	while (j < path.size())
+	{
+		int k = path[i];
+		int l = path[j];
+
+		int x = grid[l].x - grid[k].x;
+		int y = grid[l].y - grid[k].y;
+
+		if (x == 0)
+		{
+			if (y == -1)
+				directions.append("N");
+			else if (y == 1)
+				directions.append("S");
+		}
+		else if (y == 0)
+		{
+			if (x == -1)
+				directions.append("W");
+			else if (x == 1)
+				directions.append("E");
+		}
+
+		i++;
+		j++;
+	}
+
+	return directions;
+}
+
+void SpawnApple()
+{
+	do
+	{
+		apple = rand() % (COUNT * COUNT);
+	} while (cells[apple].solid);
+	cells[apple].solid = false;
+}
+
 void Update()
 {
-	if (dead)
-		return;
+	int head = snake[0];
+	int tail = snake[snake.size() - 1];
+	int north, east, south, west;
 
-	int tail = snake.size() - 1;
-	cells[snake[tail]].solid = false;
-	for (int i = tail; i > 0; i--)
-	{
+	for (int i = snake.size() - 1; i > 0; i--)
 		snake[i] = snake[i - 1];
-		cells[snake[i]].solid = true;
+
+	if (snakeAI)
+	{
+		if (snakePathIndex < snakePathDirections.length())
+		{
+			snakeDirection = snakePathDirections.at(snakePathIndex);
+			snakePathIndex++;
+		}
 	}
 
 	switch (snakeDirection)
 	{
 	case 'N':
-		if (cells[snake[0]].y > 0)
-			snake[0] -= COUNT;
+		north = head - COUNT;
+
+		if (cells[head].y > 0 && !cells[north].solid)
+		{
+			cells[tail].solid = false;
+			cells[head].solid = false;
+			snake[0] = north;
+		}
 		else
 			dead = true;
-		break;
-	case 'S':
-		if (cells[snake[0]].y < COUNT - 1)
-			snake[0] += COUNT;
-		else
-			dead = true;
-		break;
-	case 'W':
-		if (cells[snake[0]].x > 0)
-			snake[0]--;
-		else
-			dead = true;
+
 		break;
 	case 'E':
-		if (cells[snake[0]].x < COUNT - 1)
-			snake[0]++;
+		east = head + 1;
+
+		if (cells[head].x < COUNT - 1 && !cells[east].solid)
+		{
+			cells[tail].solid = false;
+			cells[head].solid = false;
+			snake[0] = east;
+		}
 		else
 			dead = true;
+
+		break;
+	case 'S':
+		south = head + COUNT;
+
+		if (cells[head].y < COUNT - 1 && !cells[south].solid)
+		{
+			cells[tail].solid = false;
+			cells[head].solid = false;
+			snake[0] = south;
+		}
+		else
+			dead = true;
+
+		break;
+	case 'W':
+		west = head - 1;
+
+		if (cells[head].x > 0 && !cells[west].solid)
+		{
+			cells[tail].solid = false;
+			cells[head].solid = false;
+			snake[0] = west;
+		}
+		else
+			dead = true;
+
 		break;
 	}
 
-	cells[snake[0]].visited = true;
-	cells[snake[0]].solid = true;
-	cells[snake[0]].direction = snakeDirection;
+	head = snake[0];
+	cells[head].direction = snakeDirection;
+	for (auto& snakePart : snake)
+		cells[snakePart].solid = true;
 
-	if (snake.size() == 2)
+	if (head == apple)
 	{
-		int head = snake[0];
-		int tail = snake[1];
+		SpawnApple();
 
-		if (cells[head].direction == 'N' && cells[tail].direction == 'S' || cells[head].direction == 'S' && cells[tail].direction == 'N')
-		{
-			snake[0] = snake[1];
-			dead = true;
-		}
-
-		if (cells[head].direction == 'W' && cells[tail].direction == 'E' || cells[head].direction == 'E' && cells[tail].direction == 'W')
-		{
-			snake[0] = snake[1];
-			dead = true;
-		}
-	}
-	else if (snake.size() > 2)
-	{
-		for (int i = 1; i < snake.size(); i++)
-		{
-			if (snake[0] == snake[i])
-				dead = true;
-		}
-	}
-
-	if (snake[0] == apple)
-	{
-		eaten = true;
-
-		do
-		{
-			apple = rand() % (COUNT * COUNT);
-		} while (cells[apple].solid);
-		cells[apple].solid = false;
-
-		int tail = snake.size() - 1;
-		switch (cells[snake[tail]].direction)
+		int tail = snake[snake.size() - 1];
+		switch (cells[tail].direction)
 		{
 		case 'N':
-			newTail = snake[tail] + COUNT;
+			newTail = tail + COUNT;
 			break;
 		case 'S':
-			newTail = snake[tail] - COUNT;
+			newTail = tail - COUNT;
 			break;
 		case 'W':
-			newTail = snake[tail] + 1;
+			newTail = tail + 1;
 			break;
 		case 'E':
-			newTail = snake[tail] - 1;
+			newTail = tail - 1;
 			break;
 		}
 
@@ -176,14 +265,11 @@ void StartGame()
 			cells[i * COUNT + j].x = j;
 			cells[i * COUNT + j].y = i;
 			cells[i * COUNT + j].solid = false;
-			cells[i * COUNT + j].visited = false;
-			cells[i + COUNT + j].direction = 'X';
 		}
 	}
 
 	snake.clear();
 	snakeLength = 0;
-
 	int center = (COUNT * COUNT) / 2;
 	if (COUNT % 2 == 0)
 	{
@@ -193,14 +279,18 @@ void StartGame()
 
 
 	snake.push_back(center);
-	cells[snake[0]].visited = true;
 	cells[snake[0]].solid = true;
 
-	do
+	SpawnApple();
+
+	snakePathIndex = 0;
+	if (snakeAI)
 	{
-		apple = rand() % (COUNT * COUNT);
-	} while (cells[apple].solid);
-	cells[apple].solid = false;
+		Cell temp[COUNT * COUNT];
+		std::copy(std::begin(cells), std::end(cells), std::begin(temp));
+		snakePath = ShortestPathBetween(snake[0], apple, temp);
+		snakePathDirections = GetPathDirections(snakePath, temp);
+	}
 }
 
 int main()
@@ -208,6 +298,7 @@ int main()
 	srand(time(NULL));
 
 	sf::RenderWindow window(sf::VideoMode(width, height), "Snake", sf::Style::Titlebar | sf::Style::Close);
+	window.setKeyRepeatEnabled(false);
 
 	emptyTexture.loadFromFile("Sprites/Empty.png");
 	appleTexture.loadFromFile("Sprites/Apple.png");
@@ -234,46 +325,65 @@ int main()
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
-		}
-
-		if (!dead)
-		{
-			if (!snakeAI)
+			else if (event.type == sf::Event::KeyPressed)
 			{
-				if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) || sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+				if (!snakeAI)
 				{
-					snakeDirection = 'N';
+					if (event.key.code == sf::Keyboard::W || event.key.code == sf::Keyboard::Up)
+					{
+						snakeDirection = 'N';
 
-					if (!gameBegan)
-						gameBegan = true;
+						if (!gameBegan)
+							gameBegan = true;
+					}
+					else if (event.key.code == sf::Keyboard::D || event.key.code == sf::Keyboard::Right)
+					{
+						snakeDirection = 'E';
+
+						if (!gameBegan)
+							gameBegan = true;
+					}
+					else if (event.key.code == sf::Keyboard::S || event.key.code == sf::Keyboard::Down)
+					{
+						snakeDirection = 'S';
+
+						if (!gameBegan)
+							gameBegan = true;
+					}
+					else if (event.key.code == sf::Keyboard::A || event.key.code == sf::Keyboard::Left)
+					{
+						snakeDirection = 'W';
+
+						if (!gameBegan)
+							gameBegan = true;
+					}
+					else if (event.key.code == sf::Keyboard::Space)
+					{
+						if (gameBegan)
+						{
+							if (dead)
+								StartGame();
+							else
+								paused = !paused;
+						}
+					}
 				}
-				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) || sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+				else
 				{
-					snakeDirection = 'E';
-
-					if (!gameBegan)
-						gameBegan = true;
-				}
-				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) || sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-				{
-					snakeDirection = 'S';
-
-					if (!gameBegan)
-						gameBegan = true;
-				}
-				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) || sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-				{
-					snakeDirection = 'W';
-
-					if (!gameBegan)
-						gameBegan = true;
+					if (event.key.code == sf::Keyboard::Space)
+					{
+						if (gameBegan)
+						{
+							if (dead)
+								StartGame();
+							else
+								paused = !paused;
+						}
+						else
+							gameBegan = true;
+					}
 				}
 			}
-		}
-		else
-		{
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
-				StartGame();
 		}
 
 		window.clear();
@@ -282,7 +392,7 @@ int main()
 		clock.restart();
 		timer += time;
 
-		if (gameBegan && !paused)
+		if (gameBegan && !paused && !dead)
 		{
 			if (timer > delay)
 			{
@@ -294,11 +404,23 @@ int main()
 			{
 				eatTimer += time;
 
-				if (eatTimer > delay * 3 || dead)
+				if (eatTimer > delay * 3)
 				{
 					eatTimer = 0;
 					eaten = false;
 				}
+			}
+		}
+
+		if (dead)
+		{
+			for (int i = 0; i < COUNT * COUNT; i++)
+				cells[i].solid = false;
+
+			if (eaten)
+			{
+				eatTimer = 0;
+				eaten = false;
 			}
 		}
 
